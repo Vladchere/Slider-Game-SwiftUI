0e3f6906f8c17467b1c0050e083b8869cd3a65fb
Vladchere <vladchere.ok@mail.ru>
2020-08-02 18:52:24 +0700

    Initial Commit

---
+++ Slider Game SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slider Game SwiftUI
+//
+//  Created by Vladislav on 02.08.2020.
+//  Copyright © 2020 Vladislav Cheremisov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
